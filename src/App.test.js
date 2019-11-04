9e1ce3435766b6ba2490b34729febdb452be7230
Noah Gallant <noah.gallant@mun.ca>
2019-11-04 01:18:56 -0330

    fixed broken unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+configure({adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const component = shallow(<App/>);
+  expect(component).toMatchSnapshot();
 });
